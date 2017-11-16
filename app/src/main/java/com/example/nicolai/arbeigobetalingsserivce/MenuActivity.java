e4c9723a3828db5ff2989c7c2934738ddebe6763
Zeatz <Nicolaisteam2@gmail.com>
2017-11-16 17:03:28 +0100

    Opdatering med ny intent

--- app/src/main/java/com/example/nicolai/arbeigobetalingsserivce/MenuActivity.java
+++ app/src/main/java/com/example/nicolai/arbeigobetalingsserivce/MenuActivity.java
@@ -1,7 +1,9 @@
 package com.example.nicolai.arbeigobetalingsserivce;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class  MenuActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class  MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+    }
+
+    public void onClickMineOpgaver(View view){
+        Intent intent = new Intent(MenuActivity.this, mineOpgaverActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/example/nicolai/arbeigobetalingsserivce/mineOpgaverActivity.java
@@ -0,0 +1,13 @@
+package com.example.nicolai.arbeigobetalingsserivce;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mineOpgaverActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mine_opgaver);
+    }
+}
