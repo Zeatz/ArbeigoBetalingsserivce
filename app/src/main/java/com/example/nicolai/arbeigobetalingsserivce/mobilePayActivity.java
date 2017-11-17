916cb56efcfc9beac532a466c8a4e0f1231d8231
Zeatz <Nicolaisteam2@gmail.com>
2017-11-17 13:48:48 +0100

    Opdatering med mobilepay

---
+++ app/src/main/java/com/example/nicolai/arbeigobetalingsserivce/mobilePayActivity.java
@@ -0,0 +1,79 @@
+package com.example.nicolai.arbeigobetalingsserivce;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import java.math.BigDecimal;
+
+import dk.danskebank.mobilepay.sdk.Country;
+import dk.danskebank.mobilepay.sdk.MobilePay;
+import dk.danskebank.mobilepay.sdk.ResultCallback;
+import dk.danskebank.mobilepay.sdk.model.FailureResult;
+import dk.danskebank.mobilepay.sdk.model.Payment;
+import dk.danskebank.mobilepay.sdk.model.SuccessResult;
+
+public class mobilePayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mobile_pay);
+        MobilePay.getInstance().init("APPDK0000000000", Country.DENMARK);
+    }
+
+    int MOBILEPAY_PAYMENT_REQUEST_CODE = 1337;
+
+
+    public void onClickBetal(View view){
+
+             // Check if the MobilePay app is installed on the device.
+            boolean isMobilePayInstalled = MobilePay.getInstance().isMobilePayInstalled(getApplicationContext());
+
+            if (isMobilePayInstalled) {
+            // MobilePay is present on the system. Create a Payment object.
+            Payment payment = new Payment();
+            payment.setProductPrice(new BigDecimal(10.0));
+            payment.setOrderId("86715c57-8840-4a6f-af5f-07ee89107ece");
+
+            // Create a payment Intent using the Payment object from above.
+            Intent paymentIntent = MobilePay.getInstance().createPaymentIntent(payment);
+
+            // We now jump to MobilePay to complete the transaction. Start MobilePay and wait for the result using an unique result code of your choice.
+            startActivityForResult(paymentIntent, MOBILEPAY_PAYMENT_REQUEST_CODE);
+            } else {
+            // MobilePay is not installed. Use the SDK to create an Intent to take the user to Google Play and download MobilePay.
+            Intent intent = MobilePay.getInstance().createDownloadMobilePayIntent(getApplicationContext());
+            startActivity(intent);
+            }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == MOBILEPAY_PAYMENT_REQUEST_CODE) {
+            // The request code matches our MobilePay Intent
+            MobilePay.getInstance().handleResult(resultCode, data, new ResultCallback() {
+                @Override
+                public void onSuccess(SuccessResult result) {
+                    // The payment succeeded - you can deliver the product.
+                }
+                @Override
+                public void onFailure(FailureResult result) {
+                    // The payment failed - show an appropriate error message to the user. Consult the MobilePay class documentation for possible error codes.
+                }
+                @Override
+                public void onCancel() {
+                    // The payment was cancelled.
+                }
+            });
+        }
+    }
+
+
+
+
+
+}
+
