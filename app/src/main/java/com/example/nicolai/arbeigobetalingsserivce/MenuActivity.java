6bb465e40b29fa0ffad9672d3da2f4dc74e89254
Zeatz <Nicolaisteam2@gmail.com>
2017-11-20 10:32:21 +0100

    Opdatering med internet

--- app/src/main/java/com/example/nicolai/arbeigobetalingsserivce/MenuActivity.java
+++ app/src/main/java/com/example/nicolai/arbeigobetalingsserivce/MenuActivity.java
@@ -1,6 +1,9 @@
 package com.example.nicolai.arbeigobetalingsserivce;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -11,10 +14,18 @@ public class  MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mobile_pay);
+        isNetworkAvailable();
     }
 
     public void onClickMineOpgaver(View view){
         Intent intent = new Intent(MenuActivity.this, mineOpgaverActivity.class);
         startActivity(intent);
+    }
+
+    private boolean isNetworkAvailable() {
+        ConnectivityManager connectivityManager
+                = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+        return activeNetworkInfo != null && activeNetworkInfo.isConnected();
     }
 }
